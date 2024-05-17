37458c1ddc3ff362c9365708943177ddc9bd9926
Arthur Barboza <barbozaarthurr@gmail.com>
2024-05-17 11:21:03 -0300

    adicionando conteúdo a TipoVariáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,21 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String meuNome = "Arthur Luiz Oliveira Barboza";
+        System.out.println(meuNome);
+
+        double salarioMinimo = 2500;
+        System.out.println(salarioMinimo);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        System.out.println(numeroCurto2);
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
     }
 }
